be2953e59256cbdb38b4fb6853f5a54592c45263
Rens Junior Sibarani <garoga.sehat@gmail.com>
2024-01-04 22:34:37 +0700

    [Revise the settings page and create a logout function]

--- app/src/main/java/com/mandiri/mandiriapps/presentation/SettingFragment.kt
+++ app/src/main/java/com/mandiri/mandiriapps/presentation/SettingFragment.kt
@@ -1,5 +1,6 @@
 package com.mandiri.mandiriapps.presentation
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,9 +9,11 @@ import androidx.fragment.app.Fragment
 import com.mandiri.mandiriapps.databinding.FragmenHomeBinding
 import com.mandiri.mandiriapps.databinding.FragmenMessageBinding
 import com.mandiri.mandiriapps.databinding.FragmentSettingBinding
+import com.mandiri.mandiriapps.helper.SharedPref
 
 class SettingFragment:Fragment() {
     private var _binding: FragmentSettingBinding? = null
+    private lateinit var sharedPref: SharedPref
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -24,10 +27,20 @@ class SettingFragment:Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        sharedPref = SharedPref(requireContext())
+        binding.componentSettingLogout.llLogout.setOnClickListener{
+            logout()
+        }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding=null
+    }
+
+    private fun logout() {
+        sharedPref.clearDataPref()
+        startActivity(Intent(requireContext(), LoginActivity::class.java))
+
     }
 }
